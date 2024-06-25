e6e19fce46050b1d849f637b65c8c1086615c16d
iaphane023 <iaphane023@student.wethinkcode.co.za>
2024-06-25 20:35:36 +0200

    resume now works, trying to add skill section

---
+++ index.js
@@ -0,0 +1,16 @@
+// /* ##### EXPERIENCE SECTION #### */
+
+//       // Check the location of each element hidden */
+//       $('.experience .content .hidden').each( function(i){
+
+//         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+//         /* If the object is completely visible in the window, fadeIn it */
+//         if( bottom_of_window > bottom_of_object ){
+
+//           $(this).animate({
+//             'opacity':'1',
+//             'margin-left': '0'
+//           },600);
+//         }
+//     });
