fc22ddc7436cf74a201c1faffff90ba786a2cb86
iaphane023 <iaphane023@student.wethinkcode.co.za>
2024-09-17 09:24:18 +0200

    structure almost complete

---
+++ function.js
@@ -0,0 +1,18 @@
+
+// window.alert("sometext");
+// $(document).ready(function() {
+//     // Check if the element is visible in the viewport
+//     $(window).on('scroll', function() {
+//         $('.experience-content').each(function() {
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+//             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            
+//             if (bottom_of_window > bottom_of_object) {
+//                 $(this).animate({
+//                     'opacity': '1',
+//                     'margin-left': '0'
+//                 }, 600); // Fades in and slides left into view
+//             }
+//         });
+//     });
+// });
