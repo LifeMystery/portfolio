62f3904f9a9efb812eb45dcecb40dbf1fee06e7e
iaphane023 <iaphane023@student.wethinkcode.co.za>
2024-10-17 07:33:35 +0200

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontfamily:{
+        BebasNeue:["Bebas Neue"]
+      }
+    },
   },
   plugins: [],
 }
